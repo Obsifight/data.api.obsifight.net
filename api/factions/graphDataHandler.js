e3ec9554744de1ae0e1bad6d2ef8a08323f2b0fb
Eywek <contact@eywek.fr>
2017-12-26 23:23:34 +0100

    Use async.series to delete old factions stats data

--- api/factions/graphDataHandler.js
+++ api/factions/graphDataHandler.js
@@ -5,77 +5,86 @@ var _ = require('underscore')
 module.exports = {
 
     generate: function () {
-        // Find faction on cache
-        databases.getMysql('cache').query('SELECT * FROM factions', function (err, factions) {
-            if (err) {
-                console.error(err)
-                return res.status(500).json({status: false, error: "Unable to get factions."})
-            }
-
-            async.each(factions, function (faction, next) {
-                // Store basics
-                databases.getMysql('cache').query('INSERT INTO stats SET faction_id = ?, position = ?, money = ?,' +
-                    'kills = ?, deaths = ?, created_at = ?', [faction.id, faction.position, faction.money, faction.kills_count,
-                    faction.deaths_count, new Date()], function (err, stats) {
+        async.series([
+            function (cb) {
+                // Find faction on cache
+                databases.getMysql('cache').query('SELECT * FROM factions', function (err, factions) {
                     if (err) {
                         console.error(err)
-                        return res.status(500).json({status: false, error: "Unable to set factions's stats."})
+                        return res.status(500).json({status: false, error: "Unable to get factions."})
                     }
-                    // Get materials data
-                    databases.getMysql('blockstats').query("SELECT material.name AS name, faction_material_count.count AS count\n" +
-                        "FROM material\n" +
-                        "LEFT JOIN faction_material_count\n" +
-                        "ON faction_material_count.material_id = material.id AND faction_material_count.faction_id = ? " +
-                        "WHERE material.name IN ('GARNET_INGOT', 'GARNET_BLOCK', 'AMETHYST_INGOT', 'AMETHYST_BLOCK', 'TITANIUM_INGOT', 'TITANIUM_BLOCK', 'OBSIDIAN_INGOT', 'OBSIDIAN_BLOCK', 'INGOT_XENOTIUM', 'XENOTIUM_BLOCK', 'TNT', 'XTNT', 'ENDER_PEARL', 'GOLDEN_APPLE')", [faction.id],
-                        function (err, rows) {
+
+                    async.each(factions, function (faction, next) {
+                        // Store basics
+                        databases.getMysql('cache').query('INSERT INTO stats SET faction_id = ?, position = ?, money = ?,' +
+                            'kills = ?, deaths = ?, created_at = ?', [faction.id, faction.position, faction.money, faction.kills_count,
+                            faction.deaths_count, new Date()], function (err, stats) {
                             if (err) {
                                 console.error(err)
-                                return
+                                return res.status(500).json({status: false, error: "Unable to set factions's stats."})
                             }
-
-                            var materials = []
-                            for (var i = 0; i < rows.length; i++) {
-                                if (rows[i].count)
-                                    materials[i] = {
-                                        name: rows[i].name,
-                                        count: rows[i].count,
-                                        stats_id: stats.insertId
+                            // Get materials data
+                            databases.getMysql('blockstats').query("SELECT material.name AS name, faction_material_count.count AS count\n" +
+                                "FROM material\n" +
+                                "LEFT JOIN faction_material_count\n" +
+                                "ON faction_material_count.material_id = material.id AND faction_material_count.faction_id = ? " +
+                                "WHERE material.name IN ('GARNET_INGOT', 'GARNET_BLOCK', 'AMETHYST_INGOT', 'AMETHYST_BLOCK', 'TITANIUM_INGOT', 'TITANIUM_BLOCK', 'OBSIDIAN_INGOT', 'OBSIDIAN_BLOCK', 'INGOT_XENOTIUM', 'XENOTIUM_BLOCK', 'TNT', 'XTNT', 'ENDER_PEARL', 'GOLDEN_APPLE')", [faction.id],
+                                function (err, rows) {
+                                    if (err) {
+                                        console.error(err)
+                                        return
                                     }
-                                else
-                                    materials[i] = {
-                                        name: rows[i].name,
-                                        count: 0,
-                                        stats_id: stats.insertId
+
+                                    var materials = []
+                                    for (var i = 0; i < rows.length; i++) {
+                                        if (rows[i].count)
+                                            materials[i] = {
+                                                name: rows[i].name,
+                                                count: rows[i].count,
+                                                stats_id: stats.insertId
+                                            }
+                                        else
+                                            materials[i] = {
+                                                name: rows[i].name,
+                                                count: 0,
+                                                stats_id: stats.insertId
+                                            }
                                     }
-                            }
 
-                            // Store it
-                            var keys = Object.keys(materials[0])
-                            var keysString = keys.join(', ')
-                            var values = []
-                            for (var i = 0; i < materials.length; i++) {
-                                values.push(_.values(materials[i]))
-                            }
+                                    // Store it
+                                    var keys = Object.keys(materials[0])
+                                    var keysString = keys.join(', ')
+                                    var values = []
+                                    for (var i = 0; i < materials.length; i++) {
+                                        values.push(_.values(materials[i]))
+                                    }
 
-                            databases.getMysql('cache').query("INSERT INTO materials_stats (" + keysString + ") VALUES ?", [values], function (err) {
-                                if (err)
-                                    return console.error(err)
-                                next()
-                            })
+                                    databases.getMysql('cache').query("INSERT INTO materials_stats (" + keysString + ") VALUES ?", [values], function (err) {
+                                        if (err)
+                                            return console.error(err)
+                                        next()
+                                    })
+                                })
+                        })
+                    }, function () {
+                        databases.closeMysql('blockstats')
+                        cb()
                     })
                 })
-            }, function () {
-                databases.closeMysql('cache')
-                databases.closeMysql('blockstats')
-            })
+            },
+            function (cb) {
+                // Remove old (1 week max)
+                databases.getMysql('cache').query('DELETE stats, materials_stats FROM stats ' +
+                    'LEFT JOIN materials_stats ON materials_stats.stats_id = stats.id ' +
+                    'WHERE stats.created_at <= DATE_SUB(NOW(), INTERVAL 1 WEEK);', function (err) {
+                    if (err)
+                        console.error(err)
+                    cb()
+                });
+            }
+        ], function () {
+            databases.closeMysql('cache');
         })
-        // Remove old (1 week max)
-        databases.getMysql('cache').query('DELETE stats, materials_stats FROM stats ' +
-            'LEFT JOIN materials_stats ON materials_stats.stats_id = stats.id ' +
-            'WHERE stats.created_at <= DATE_SUB(NOW(), INTERVAL 1 WEEK);', function (err) {
-            if (err)
-                console.error(err)
-        });
     },
 
     /*
